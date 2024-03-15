26a5276722d1087d22632a8b1fc0e8f9c93eaba8
Stas Zholobov <lesstergy@gmail.com>
2024-03-15 13:08:49 +0300

    Declare py scripts

---
+++ tool/remove_old_from_json.py
@@ -0,0 +1,31 @@
+import os
+
+
+def remove_lines_with_filenames(file_path, directory_path):
+    filenames = []
+    for root, _, files in os.walk(directory_path):
+        for file_name in files:
+            filenames.append(file_name)
+
+    lines_to_remove = []
+    with open(file_path, "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            for filename in filenames:
+                if filename in line:
+                    print(line)
+                    lines_to_remove.append(line)
+
+    updated_lines = [line for line in lines if line not in lines_to_remove]
+
+    with open(file_path, "w") as file:
+        file.writelines(updated_lines)
+
+    print(f"Файл обновлен: {file_path}")
+
+
+# Пример использования
+directory_path = "./mod/src/WarOfTheRing/Components_Bag/CharacterUnits"
+json_file_path = "./mod/src/WarOfTheRing.json"
+
+remove_lines_with_filenames(json_file_path, directory_path)
---
+++ tool/replace_symbols.py
@@ -0,0 +1,34 @@
+import os
+
+
+def replace_characters_in_filenames(directory_path, replacements):
+    for root, _, files in os.walk(directory_path):
+        for file_name in files:
+            if not has_characters(file_name, replacements):
+                continue
+
+            original_file_path = os.path.join(root, file_name)
+            replaced_file_name = replace_characters(file_name, replacements)
+            replaced_file_path = os.path.join(root, replaced_file_name)
+            os.rename(original_file_path, replaced_file_path)
+            print(f"Файл переименован: {file_name} -> {replaced_file_name}")
+
+
+def has_characters(line, replacements):
+    for old_char, new_char in replacements.items():
+        if old_char in line:
+            return True
+    return False
+
+
+def replace_characters(line, replacements):
+    for old_char, new_char in replacements.items():
+        line = line.replace(old_char, new_char)
+    return line
+
+
+# Пример использования
+directory_path = "./mod"
+replacements = {"é": "e", "ú": "u", "ó": "o"}
+
+replace_characters_in_filenames(directory_path, replacements)
