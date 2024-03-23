51d9c4279c6f66176566951e4dad7f1ac1e5f002
Stas Zholobov <lesstergy@gmail.com>
2024-03-23 12:12:27 +0300

    Replace Character Cards to folder

--- tool/python/find_and_replace_run.py
+++ tool/python/find_and_replace_run.py
@@ -2,8 +2,8 @@ from find_text_files_by_pattern import find_files_by_text
 from replace_files import replace_files
 
 source_directory = "./mod/src/WarOfTheRing"
-destination_directory = "./mod/src/WarOfTheRing/ControlMarkers"
-pattern = '"Description": "SettlementControlMarker;'
+destination_directory = "./mod/src/WarOfTheRing/CharacterCards"
+pattern = '"Description": "Card;Minion'
 files_extension = ".json"
 
 file_paths = find_files_by_text(source_directory, pattern, files_extension)
