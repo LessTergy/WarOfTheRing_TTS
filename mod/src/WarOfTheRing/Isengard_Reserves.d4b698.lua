f02c9c139a0337864c09842961b720fe1d7057b4
Stas Zholobov <lesstergy@gmail.com>
2024-05-13 15:58:52 +0300

    Update SH reserve sfx buttons

--- mod/src/WarOfTheRing/Isengard_Reserves.d4b698.lua
+++ mod/src/WarOfTheRing/Isengard_Reserves.d4b698.lua
@@ -1,57 +1,48 @@
 function onLoad(save_state)
     self.clearButtons()
-    self.createButton({
-        click_function = "PlaySoundEffect",
-        function_owner = self,
-        label = ">",
-        position = { -2.3, 0.05, 0 },
-        rotation = { 0, 180, 0 },
-        color = { 0.83, 0.78, 0.21 },
-        width = 250,
-        height = 250,
-        font_size = 250,
-        font_color = { 0, 0, 0 },
-        tooltip =
-        "play Isengard Army sound effect."
-    })
-    self.createButton({
-        click_function = "PlaySoundEffect2",
-        function_owner = self,
-        label = ">",
-        position = { -2.3, 0.05, 0.5 },
-        rotation = { 0, 180, 0 },
-        color = { 0.83, 0.78, 0.21 },
-        width = 250,
-        height = 250,
-        font_size = 250,
-        font_color = { 0, 0, 0 },
-        tooltip =
-        "We have work to do."
-    })
-    self.createButton({
+
+    local buttonSfxArmy = CreateButtonData(-0.5)
+    buttonSfxArmy.click_function = "PlaySoundEffect"
+    buttonSfxArmy.tooltip = "Isengard Army sound effect"
+    self.createButton(buttonSfxArmy)
+
+    local buttonSfxWork = CreateButtonData(0)
+    buttonSfxWork.click_function = "PlaySoundEffect2"
+    buttonSfxWork.tooltip = "We have work to do"
+    self.createButton(buttonSfxWork)
+
+    local buttonSfxDawn = CreateButtonData(0.5)
+    buttonSfxDawn.click_function = "PlaySoundEffect3"
+    buttonSfxDawn.tooltip = "No dawn for men"
+    self.createButton(buttonSfxDawn)
+end
+
+function CreateButtonData(PositionZ)
+    return {
         click_function = "PlaySoundEffect3",
         function_owner = self,
         label = ">",
-        position = { -2.3, 0.05, -0.5 },
+        position = { -2.5, 0.05, PositionZ },
         rotation = { 0, 180, 0 },
-        color = { 0.83, 0.78, 0.21 },
+        color = { 0.914, 0.886, 0.345 },
+        font_color = { 0.169, 0.141, 0.141 },
         width = 250,
         height = 250,
         font_size = 250,
-        font_color = { 0, 0, 0 },
-        tooltip =
-        "No dawn for men."
-    })
+    }
 end
 
 function PlaySoundEffect()
-    Global.call("PlaySound", { ID = 18 }) --isengard horn
+    -- horn
+    Global.call("PlaySound", { ID = 18 })
 end
 
 function PlaySoundEffect2()
-    Global.call("PlaySound", { ID = 12 }) --we have work to do
+    -- we have work to do
+    Global.call("PlaySound", { ID = 12 })
 end
 
 function PlaySoundEffect3()
-    Global.call("PlaySound", { ID = 11 }) --no dawn for men
+    -- no dawn for men
+    Global.call("PlaySound", { ID = 11 })
 end
--- mod/src/WarOfTheRing/Nazgul_Reserves.da9dcb.lua
+++ mod/src/WarOfTheRing/Nazgul_Reserves.da9dcb.lua
@@ -4,14 +4,14 @@ function onLoad(save_state)
         click_function = "PlaySoundEffect",
         function_owner = self,
         label = ">",
-        position = { -2.9, 0.05, 0 },
+        position = { -2.5, 0.05, 0 },
         rotation = { 0, 180, 0 },
-        color = { 0, 0, 0 },
+        color = { 0.169, 0.141, 0.141 },
         width = 250,
         height = 250,
         font_size = 250,
         font_color = { 1, 0.5, 0.25 },
-        tooltip = "Scream."
+        tooltip = "Scream"
     })
 end
 
