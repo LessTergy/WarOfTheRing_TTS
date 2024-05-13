4a05e01dbbb25e367a8e17fe6365927e7fd31fed
Stas Zholobov <lesstergy@gmail.com>
2024-05-13 23:59:03 +0300

    Update Nazgul Reserves

--- mod/src/WarOfTheRing/Reserves/Nazgul_Reserves.da9dcb.lua
+++ mod/src/WarOfTheRing/Reserves/Nazgul_Reserves.da9dcb.lua
@@ -7,10 +7,10 @@ function onLoad(save_state)
         position = { -2.5, 0.05, 0 },
         rotation = { 0, 180, 0 },
         color = { 0.169, 0.141, 0.141 },
+        font_color = { 1, 0.5, 0.25 },
         width = 250,
         height = 250,
         font_size = 250,
-        font_color = { 1, 0.5, 0.25 },
         tooltip = "Scream"
     })
 end
