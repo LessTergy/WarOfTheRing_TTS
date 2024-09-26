7298e9c6db22ef8f41676a44eb6000865a0446ec
Stas Zholobov <lesstergy@gmail.com>
2024-09-26 16:53:45 +0300

    Update python tools

--- tool/python/remove_file_names_from_json.py
+++ tool/python/remove_file_names_from_json.py
@@ -1,7 +1,7 @@
 import os
 
 
-def remove_file_names_from_json(file_path, directory_path):
+def remove_folder_from_json(json_path, directory_path):
     filenames = []
     for _, _, files in os.walk(directory_path):
         for file_name in files:
@@ -9,7 +9,7 @@ def remove_file_names_from_json(file_path, directory_path):
 
     lines_to_remove = []
 
-    with open(file_path, "r") as file:
+    with open(json_path, "r") as file:
         lines = file.readlines()
         for line in lines:
             for filename in filenames:
@@ -20,7 +20,28 @@ def remove_file_names_from_json(file_path, directory_path):
     # Keep only lines, which doesn't match with lines_to_remove
     updated_lines = [line for line in lines if line not in lines_to_remove]
 
-    with open(file_path, "w") as file:
+    with open(json_path, "w") as file:
         file.writelines(updated_lines)
 
-    print(f"File updated: {file_path}")
+    print(f"File updated: {json_path}")
+
+def remove_files_from_json(json_path, file_paths):
+    lines_to_remove = []
+
+    with open(json_path, "r") as file:
+        lines = file.readlines()
+
+        for line in lines:
+            for path in file_paths:
+                filename = os.path.basename(path)
+                if filename in line:
+                    print(line)
+                    lines_to_remove.append(line)
+
+    # Keep only lines, which doesn't match with lines_to_remove
+    updated_lines = [line for line in lines if line not in lines_to_remove]
+
+    with open(json_path, "w") as file:
+        file.writelines(updated_lines)
+
+    print(f"File updated: {json_path}")
