01c811d0547103cf9bc5dd09fee16be6175fa73d
Stas Zholobov <lesstergy@gmail.com>
2024-10-03 00:20:04 +0300

    Fix order issue in all decks

--- tool/python/fix_deck_order.py
+++ tool/python/fix_deck_order.py
@@ -1,10 +1,14 @@
 import json
+from functools import partial
 
-file_path = './mod/src/WarOfTheRing/Shadow_Strategy_Event_Deck.6508fd.json'
+from find_text_files_by_pattern import find_files_by_text
+
+source_directory = "./mod/src/WarOfTheRing"
+files_extension = ".json"
 card_id_block_name = "CardID"
 
-def extract_number_from_card(card_name):
-    folder_path = get_deck_folder_path()
+def extract_number_from_card(card_name, deck_file_path):
+    folder_path = get_deck_folder_path(deck_file_path)
     file_path = folder_path + card_name
 
     with open(file_path, "r+") as file:
@@ -13,28 +17,33 @@ def extract_number_from_card(card_name):
         cardId = object_block[card_id_block_name]
         return cardId
     
-def get_deck_folder_path():
-    path_parts = file_path.split('.')
+def get_deck_folder_path(deck_file_path):
+    path_parts = deck_file_path.split('.')
     folder_path = '.' + path_parts[1] + '/'
     return folder_path
 
 def run():
-    with open(file_path, 'r') as file:
-        json_data = json.load(file)
-        object_block = json_data["Object"]
-        
-        deck_ids = object_block.get('DeckIDs', [])
-        deck_ids.sort()
+    pattern = '"DeckIDs": ['
+    file_paths = find_files_by_text(source_directory, pattern, files_extension, True)
 
-        object_block['DeckIDs'] = deck_ids
-        json_data["Object"] = object_block
+    for file_path in file_paths:
+        with open(file_path, 'r') as file:
+            json_data = json.load(file)
+            object_block = json_data["Object"]
+            
+            deck_ids = object_block.get('DeckIDs', [])
+            deck_ids.sort()
 
-        card_names = json_data.get('ContainedObjectPaths', [])
-        sorted_file_names = sorted(card_names, key=extract_number_from_card)
+            object_block['DeckIDs'] = deck_ids
+            json_data["Object"] = object_block
 
-        json_data['ContainedObjectPaths'] = sorted_file_names
+            card_names = json_data.get('ContainedObjectPaths', [])
+            key_function = partial(extract_number_from_card, deck_file_path=file_path)
+            sorted_file_names = sorted(card_names, key=key_function)
 
-        with open(file_path, 'w') as output_file:
-            json.dump(json_data, output_file, indent=4)
+            json_data['ContainedObjectPaths'] = sorted_file_names
+
+            with open(file_path, 'w') as output_file:
+                json.dump(json_data, output_file, indent=4)
 
 run()
